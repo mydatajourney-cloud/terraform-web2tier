0fb6c32bd1eec0febcf05a2217458f71cf2cccbb
mydatajourney-cloud <tranvinhtuan363@gmail.com>
2024-09-14 12:31:11 +0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -45,7 +45,7 @@ resource "aws_db_instance" "mysql-rds" {
   engine_version       = "8.0.35"
   instance_class       = "db.t3.micro"
   username             = "admin"
-  password             = "Lolaboba123$"
+  password             = "yourpass"
   skip_final_snapshot  = true
   backup_retention_period = 7
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
